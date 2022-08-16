ca1c1f23974d283cfe4a3cce93c42b16a7c39e4f
Leonardo Guimarães <leodejandira@gmail.com>
2022-08-16 11:00:43 -0300

    Aplicada padronização no Form de cadastro de clientes, conforme layout aplicado na tela principal e Update

--- src/FormCadastroCliente.java
+++ src/FormCadastroCliente.java
@@ -13,6 +13,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JLayeredPane;
+import javax.swing.ImageIcon;
 
 public class FormCadastroCliente extends JFrame {
 
@@ -51,43 +52,43 @@ public class FormCadastroCliente extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Nome completo");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel.setBounds(31, 34, 114, 25);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel.setBounds(10, 116, 114, 25);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Email");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_1.setBounds(31, 109, 77, 25);
+		JLabel lblNewLabel_1 = new JLabel("E-mail");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(10, 181, 59, 25);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("CPF");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(243, 37, 45, 19);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(10, 151, 45, 19);
 		contentPane.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Telefone");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_3.setBounds(243, 115, 77, 19);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(221, 151, 77, 19);
 		contentPane.add(lblNewLabel_3);
 		
 		textNome = new JTextField();
-		textNome.setBounds(31, 62, 183, 25);
+		textNome.setBounds(117, 117, 295, 25);
 		contentPane.add(textNome);
 		textNome.setColumns(10);
 		
 		textEmail = new JTextField();
 		textEmail.setColumns(10);
-		textEmail.setBounds(31, 138, 183, 25);
+		textEmail.setBounds(85, 181, 327, 25);
 		contentPane.add(textEmail);
 		
 		textTelefone = new JTextField();
 		textTelefone.setColumns(10);
-		textTelefone.setBounds(243, 138, 146, 25);
+		textTelefone.setBounds(279, 149, 133, 25);
 		contentPane.add(textTelefone);
 		
 		textCpf = new JTextField();
 		textCpf.setColumns(10);
-		textCpf.setBounds(243, 62, 146, 25);
+		textCpf.setBounds(42, 149, 169, 25);
 		contentPane.add(textCpf);
 		
 		// cadastra os clientes 
@@ -96,7 +97,7 @@ public class FormCadastroCliente extends JFrame {
 		
 		JButton btnCadastrarCliente = new JButton("Cadastrar Cliente");
 		btnCadastrarCliente.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnCadastrarCliente.setBounds(68, 196, 146, 31);
+		btnCadastrarCliente.setBounds(44, 219, 146, 31);
 		
 		btnCadastrarCliente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -121,7 +122,7 @@ public class FormCadastroCliente extends JFrame {
 		
 		JButton btnLimparDados = new JButton("Limpar dados");
 		btnLimparDados.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnLimparDados.setBounds(224, 196, 146, 31);
+		btnLimparDados.setBounds(235, 219, 146, 31);
 		
 		btnLimparDados.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -133,5 +134,10 @@ public class FormCadastroCliente extends JFrame {
 		});
 		
 		contentPane.add(btnLimparDados);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon(FormCadastroCliente.class.getResource("/ViewImagens/PlanoFuntoTelaUpdate.jpg")));
+		lblNewLabel_4.setBounds(-11, 0, 455, 271);
+		contentPane.add(lblNewLabel_4);
 	}
 }
